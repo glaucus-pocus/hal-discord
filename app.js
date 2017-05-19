bdd2209e7986a6bf4003110d7a00aab2509b3f25
Pandraghon <ryuunushi@gmail.com>
2017-05-20 00:16:46 +0200

    Add Sqlite + Exp for users

--- app.js
+++ app.js
@@ -3,87 +3,118 @@ const client = new Discord.Client();
 const settings = require('./settings.json');
 const secret = require('./secret.json');
 const Emoji = require("discord-emoji");
+const sql = require('sqlite');
+
+sql.open('./db.sqlite');
 
 client.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready!');
 });
+
+const updateUserXP = (points, userId) => {
+    sql.get(`SELECT xp, level FROM users WHERE id = '${userId}'`).then(row => {
+        if (!row) {
+            sql.run('INSERT INTO users (id, xp, level) VALUES (?, ?, ?)', [userId, 1, 0]);
+        } else {
+            sql.run(`UPDATE users SET xp = ${row.xp + points} WHERE id = ${userId}`)
+        }
+    }).catch(() => {
+        console.error;
+        sql.run('CREATE TABLE IF NOT EXISTS users (id TEXT, xp INTEGER, level INTEGER)').then(() => {
+            sql.run('INSERT INTO users (id, xp, level) VALUES (?, ?, ?)', [userId, 1, 0]);
+        });
+    });
+};
 
 client.on('message', message => {
     let matches;
-  if (/^ping$/i.test(message.content) /*&& !/^(?:236198969019990018|220333308569976834)$/.test(message.author.id)*/) {
-    message.channel.send('pong', {tts: true});
-  }
-  if (/my avatar/i.test(message.content)) {
-    message.reply(message.author.avatarURL);
-  }
-  if (/(?:^| )ch?ats?(?: |$)/i.test(message.content)) {
-    message.react(Emoji.nature.cat);
-  }
-  if (/(?:^| )(?:chien|dog)s?(?: |$)/i.test(message.content)) {
-    message.react(Emoji.nature.dog);
-  }
-  if (/\(\╯\°\□\°\）\╯\︵\ \┻\━\┻/.test(message.content)) {
-      message.reply('PUT. THE. TABLE. BACK.');
-  }
-  if ((matches = message.content.match(/^([a-zA-Z0-9])$/))) {
-      let c = message.content.charCodeAt(0);
-        switch(c){
-            case 'Z': c = 'A';
-            case 'z': c = 'a';
-            default: c = String.fromCharCode(++c);
-        }
-      message.reply(c);
-  }
 
-  if (message.content.startsWith(settings.command_prefix.admin) && message.member.roles.some(role => ['admin', 'superadmin'].includes(role.name) )) {
-      const command = message.content.substring(settings.command_prefix.admin.length);
-      if (command.startsWith(`purge`)) {
-          message.channel.fetchMessages()
-            .then(messages => message.channel.bulkDelete(messages))
-            .catch(error => {
-                const channel = message.guild.channels.find('name', settings.logs);
-                if (!channel) return;
-                channel.send(`Error while purging ${message.channel} : \`${error}\``);
-            });
-      }
-  }
+    if (message.channel.type === 'text') {
+        updateUserXP(1, message.author.id);
+    }
 
-  if (message.content.startsWith(settings.command_prefix.user) && message.member.roles.some(role => ['girl', 'boy', 'bot'].includes(role.name) )) {
-      const command = message.content.substring(settings.command_prefix.user.length);
-      if ((matches = command.match(/^poll \[(.*)\] ?(?:\((.*)\))?/))) {
-        const title = matches[1];
-        const options = matches[2] ? matches[2].split(' ;; ') : [];
-        let msg = `**${title}**`;
-        for (let option in options) {
-            msg += `\n **${Number(option) + 1}**. \`${options[option]}\``;
+    if (/^ping$/i.test(message.content) /*&& !/^(?:236198969019990018|220333308569976834)$/.test(message.author.id)*/ ) {
+        message.channel.send('pong', { tts: true });
+    }
+    if (/my avatar/i.test(message.content)) {
+        message.reply(message.author.avatarURL);
+    }
+    if (/(?:^| )ch?ats?(?: |$)/i.test(message.content)) {
+        message.react(Emoji.nature.cat);
+    }
+    if (/(?:^| )(?:chien|dog)s?(?: |$)/i.test(message.content)) {
+        message.react(Emoji.nature.dog);
+    }
+    if (/\(\╯\°\□\°\）\╯\︵\ \┻\━\┻/.test(message.content)) {
+        message.reply('PUT. THE. TABLE. BACK.');
+    }
+    if ((matches = message.content.match(/^([a-zA-Z0-9])$/))) {
+        let c = message.content.charCodeAt(0);
+        switch (c) {
+            case 'Z':
+                c = 'A';
+            case 'z':
+                c = 'a';
+            default:
+                c = String.fromCharCode(++c);
         }
-        message.channel.send(msg)
-            .then(msg => {
-                const emojiList = {
-                    1: 'one',
-                    2: 'two',
-                    3: 'three',
-                    4: 'four',
-                    5: 'five',
-                    6: 'six',
-                    7: 'seven',
-                    8: 'eight',
-                    9: 'nine'
-                };
-                if (options.length > 0) {
-                    let promise = msg.react(Emoji.symbols[emojiList[1]]);
-                    for (let option in options) {
-                        promise = promise.then(react => react.message.react(Emoji.symbols[emojiList[Number(option) + 2]]));
+        message.reply(c);
+    }
+
+    if (message.content.startsWith(settings.command_prefix.admin) && message.member.roles.some(role => ['admin', 'superadmin'].includes(role.name))) {
+        const command = message.content.substring(settings.command_prefix.admin.length);
+        if (command.startsWith(`purge`)) {
+            message.channel.fetchMessages()
+                .then(messages => message.channel.bulkDelete(messages))
+                .catch(error => {
+                    const channel = message.guild.channels.find('name', settings.logs);
+                    if (!channel) return;
+                    channel.send(`Error while purging ${message.channel} : \`${error}\``);
+                });
+        }
+    }
+
+    if (message.content.startsWith(settings.command_prefix.user) && message.member.roles.some(role => ['girl', 'boy', 'bot'].includes(role.name))) {
+        const command = message.content.substring(settings.command_prefix.user.length);
+        if ((matches = command.match(/^poll \[(.*)\] ?(?:\((.*)\))?/))) {
+            const title = matches[1];
+            const options = matches[2] ? matches[2].split(' ;; ') : [];
+            let msg = `**${title}**`;
+            for (let option in options) {
+                msg += `\n **${Number(option) + 1}**. \`${options[option]}\``;
+            }
+            message.channel.send(msg)
+                .then(msg => {
+                    const emojiList = {
+                        1: 'one',
+                        2: 'two',
+                        3: 'three',
+                        4: 'four',
+                        5: 'five',
+                        6: 'six',
+                        7: 'seven',
+                        8: 'eight',
+                        9: 'nine'
+                    };
+                    if (options.length > 0) {
+                        let promise = msg.react(Emoji.symbols[emojiList[1]]);
+                        for (let option in options) {
+                            promise = promise.then(react => react.message.react(Emoji.symbols[emojiList[Number(option) + 2]]));
+                        }
                     }
-                }
-            })
-            .catch(error => {
-                const channel = message.guild.channels.find('name', settings.logs);
-                if (!channel) return;
-                channel.send(`Error while creating poll in ${message.channel} : \`${error}\`\nCommand : \`${message.content}\``);
+                })
+                .catch(error => {
+                    const channel = message.guild.channels.find('name', settings.logs);
+                    if (!channel) return;
+                    channel.send(`Error while creating poll in ${message.channel} : \`${error}\`\nCommand : \`${message.content}\``);
+                });
+        } else if (command === 'lvl') {
+            sql.get(`SELECT xp, level FROM users WHERE id = '${message.author.id}'`).then(row => {
+                if (!row) return message.reply('Ton niveau actuel est ... 0.');
+                message.reply(`Ton niveau actuel est ${row.level} [${row.xp}]`);
             });
-      }
-  }
+        }
+    }
 });
 
 client.on('guildMemberAdd', member => {
@@ -107,13 +138,13 @@ client.on('guildMemberRemove', member => {
 });
 
 client.on('guildMemberUpdate', (oldMember, newMember) => {
-    const channel = newMember.guild.channels.find('name', settings.logs);
-    if (!channel) return;
-    const newRoles = newMember.roles.filter(role => !oldMember.roles.has(role.id)).map(role => role.name);
-    const removedRoles = oldMember.roles.filter(role => !newMember.roles.has(role.id)).map(role => role.name);
+            const channel = newMember.guild.channels.find('name', settings.logs);
+            if (!channel) return;
+            const newRoles = newMember.roles.filter(role => !oldMember.roles.has(role.id)).map(role => role.name);
+            const removedRoles = oldMember.roles.filter(role => !newMember.roles.has(role.id)).map(role => role.name);
 
-    let message = `${oldMember} changed :`;
-    if (newRoles.length > 0) message += `\n - New roles : \`${newRoles.join('\`, \`')}\``;
+            let message = `${oldMember} changed :`;
+            if (newRoles.length > 0) message += `\n - New roles : \`${newRoles.join('\`, \`')}\``;
     if (removedRoles.length > 0) message += `\n - Removed roles : \`${removedRoles.join('\`, \`')}\``;
     
     channel.send(message);
