817c09e27729206ff6a977b2ab7220a5502f1dc0
Pandraghon <ryuunushi@gmail.com>
2017-07-19 22:27:07 +0200

    Clean kick command

--- commands/Admin/kick.js
+++ commands/Admin/kick.js
@@ -1,31 +1,17 @@
-exports.run = (client, message, args) => {
-	if (message.mentions.users.size === 0) {
-		return message.reply('T\'as pas dit qui qu\'tu veux kick').catch(console.error);
-	}
-	const kickMember = message.guild.member(message.mentions.users.first());
-	if (!kickMember) {
-		return message.reply('Hum, c\'est qui ? Connais pas.');
-	}
-	if (!message.guild.member(client.user).hasPermission('KICK_MEMBERS')) {
-		return message.reply('Je peux pas.').catch(console.error);
-	}
-	kickMember.kick().then((member) => {
-		message.reply(`${member.user.username} a dégagé.`).catch(console.error);
-	}).catch(console.error);
-};
+exports.run = async (client, message, [member]) => member.kick().then(() => message.markAsDone());
 
 exports.conf = {
 	runIn: ['text'],
-	enabled: false,
+	enabled: true,
 	aliases: [],
 	permLevel: 5,
-	botPerms: [],
+	botPerms: ['KICK_MEMBERS'],
 	nsfw: false
 };
 
 exports.help = {
 	name: 'kick',
 	description: 'Kicks a member',
-	usage: '<member>',
+	usage: '<member:member>',
 	usageDelim: '',
 };
