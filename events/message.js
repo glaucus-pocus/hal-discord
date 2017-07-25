2c278631d6cbb355652e0482765d9b2e0a886f7a
Pandraghon <ryuunushi@gmail.com>
2017-07-25 22:12:30 +0200

    Clean message event

--- events/message.js
+++ events/message.js
@@ -1,31 +1,9 @@
-//const sql = require('sqlite');
 const Emoji = require('discord-emoji');
 const settings = require('../settings.json');
-
-//sql.open('../db.sqlite');
-
-const updateUserXP = (points, userId) => {
-	sql.get(`SELECT xp, level FROM users WHERE id = '${userId}'`).then((row) => {
-		if (!row) {
-			sql.run('INSERT INTO users (id, xp, level) VALUES (?, ?, ?)', [userId, 1, 0]);
-		} else {
-			sql.run(`UPDATE users SET xp = ${row.xp + points} WHERE id = ${userId}`);
-		}
-	}).catch(() => {
-		console.error;
-		sql.run('CREATE TABLE IF NOT EXISTS users (id TEXT, xp INTEGER, level INTEGER)').then(() => {
-			sql.run('INSERT INTO users (id, xp, level) VALUES (?, ?, ?)', [userId, 1, 0]);
-		});
-	});
-};
 
 exports.run = async (client, message) => {
 	if (!client.ready) return;
 	let matches;
-
-	/*if (message.channel.type === 'text') {
-		updateUserXP(1, message.author.id);
-	}*/
 
 	if (/^ping$/i.test(message.content) /*&& !/^(?:236198969019990018|220333308569976834)$/.test(message.author.id)*/ ) {
 		message.channel.send('pong', { tts: true });
