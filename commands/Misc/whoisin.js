833b5b3480b1002d632004c29a88b6ed87290745
Pandraghon <ryuunushi@gmail.com>
2017-08-15 21:40:00 +0200

    Added requiredProviders inhibitor and whoisin command

---
+++ commands/Misc/whoisin.js
@@ -0,0 +1,18 @@
+exports.run = async (client, message, [role]) => message.send(`List of ${role.name} members: ${role.members.map(m => m.displayName).join(', ')}`);
+
+exports.conf = {
+  runIn: ['text'],
+  enabled: true,
+  aliases: [],
+  permLevel: 1,
+  botPerms: [],
+  nsfw: false,
+  cooldown: 5,
+};
+
+exports.help = {
+  name: 'whoisin',
+  description: 'Enumerates the members having the role.',
+  usage: '<role:role>',
+  usageDelim: '',
+};
